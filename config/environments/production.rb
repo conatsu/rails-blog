ae8674595aa024323f3aefb962df46d2efb8e7d0
tmomomo <61401716+conatsu@users.noreply.github.com>
2023-11-21 06:25:00 +0000

    add CRUD

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['http://localhost:3000']
+
 end
